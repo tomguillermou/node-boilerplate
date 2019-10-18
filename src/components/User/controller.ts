79a6d30502d64aab35c5965cbb7cc2dbb4cdf7b1
tomguillermou <tomguillermou@gmail.com>
2019-10-18 14:36:46 +0200

    Implement filters on attributes for the read many request

--- src/components/User/controller.ts
+++ src/components/User/controller.ts
@@ -7,7 +7,27 @@ import { handleErrorReponse } from "../../core/errors";
 export async function readMany(req: Request, res: Response) {
 
     try {
-        const users = await User.find().exec();
+        const { firstname, lastname, position, email } = req.query;
+
+        const query = User.find();
+
+        if (firstname) {
+            query.where("firstname").equals(firstname);
+        }
+
+        if (lastname) {
+            query.where("lastname").equals(lastname);
+        }
+
+        if (position) {
+            query.where("position").equals(position);
+        }
+
+        if (email) {
+            query.where("email").equals(email);
+        }
+
+        const users = await query.exec();
 
         res.json({ data: users });
 
