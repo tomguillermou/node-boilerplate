0032c3cb40af529fa74ef4927ee85bf5d638f5bd
tomguillermou <t.guillermou@citron.io>
2021-04-13 16:51:46 +0200

    ⚡️ Update loading of environment variables

---
+++ src/config/configuration.ts
@@ -0,0 +1,8 @@
+export const environmentVariablesList: string[] = [
+    'ENVIRONMENT',
+    'PORT',
+    'MONGO_URI',
+    'MONGO_DATABASE',
+    'JWT_SECRET',
+    'SALT_ROUNDS',
+];
--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -1,32 +1,26 @@
 import dotenv from 'dotenv';
 import fs from 'fs';
 
-const config = {
-    customPath: '.env',
-    defaultPath: '.env.example',
-};
+import { environmentVariablesList } from '@config';
 
-/** Environment variables list */
-const variables = [
-    'ENVIRONMENT',
-    'PORT',
-    'MONGO_URI',
-    'MONGO_DATABASE',
-    'JWT_SECRET',
-    'SALT_ROUNDS',
-];
+const envPath = {
+    defaultPath: '.env',
+    defaultExamplePath: '.env.example',
+};
 
 export function load(): void {
     // Detect file path to load variables from
-    const path = fs.existsSync(config.customPath) ? config.customPath : config.defaultPath;
+    const path = fs.existsSync(envPath.defaultPath)
+        ? envPath.defaultPath
+        : envPath.defaultExamplePath;
     console.log(`INFO: Reading environment variables from ${path} file`);
 
     // Load environment variables
     dotenv.config({ path });
 
-    // Make sure every variables are loaded or exit process
-    for (const variable of variables) {
-        if (typeof process.env[variable] !== 'string') {
+    // Make sure each environment variable is defined, otherwise exit process
+    for (const variable of environmentVariablesList) {
+        if (typeof process.env[variable] === 'undefined') {
             console.error('ERROR: Missing an environment variable');
             process.exit(1);
         }
