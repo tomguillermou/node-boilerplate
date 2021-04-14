bb0218808a129ae258e9082bf0da0fb7e5a11bb7
tomguillermou <t.guillermou@citron.io>
2021-04-14 11:41:07 +0200

    👌 Implement requested changes

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,4 +1,4 @@
-export const environmentVariablesList: string[] = [
+export const SECRETS = [
     'ENVIRONMENT',
     'PORT',
     'MONGO_URI',
--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import fs from 'fs';
 
-import { environmentVariablesList } from '@config';
+import { SECRETS } from '@config';
 
 const envPath = {
     defaultPath: '.env',
@@ -9,20 +9,26 @@ const envPath = {
 };
 
 export function load(): void {
-    // Detect file path to load variables from
+    // Detect file path to load secrets from
     const path = fs.existsSync(envPath.defaultPath)
         ? envPath.defaultPath
         : envPath.defaultExamplePath;
-    console.log(`INFO: Reading environment variables from ${path} file`);
+    console.log(`[log] Loading secrets from ${path} file.`);
 
     // Load environment variables
     dotenv.config({ path });
 
-    // Make sure each environment variable is defined, otherwise exit process
-    for (const variable of environmentVariablesList) {
-        if (typeof process.env[variable] === 'undefined') {
-            console.error('ERROR: Missing an environment variable');
-            process.exit(1);
+    // Make sure no secret is missing, otherwise exit process
+    let missingSecret = false;
+
+    for (const secret of SECRETS) {
+        if (typeof process.env[secret] === 'undefined' || !process.env[secret]) {
+            console.error(`[error] ${secret} is missing from ${path} file.`);
+            missingSecret = true;
         }
+    }
+
+    if (missingSecret) {
+        process.exit(1);
     }
 }
